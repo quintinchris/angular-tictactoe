aeca42c06a8e5a7210d89b8ce3d18d0d358c6d1e
chrisq32 <cwq23drexel@gmail.com>
2020-05-28 20:25:07 -0400

    Added Nebula styling

---
+++ src/app/square/square.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-square',
+  template: `
+    <button nbButton *ngIf="!value">{{ value }}</button>
+    <button nbButton hero status="success" *ngIf="value == 'X'">{{ value }}</button>
+    <button nbButton hero status="info" *ngIf="value == 'O'">{{ value }}</button>
+  `,
+  styles: [
+  ]
+})
+export class SquareComponent {
+
+  @Input() value: 'X' | 'O';
+
+}
